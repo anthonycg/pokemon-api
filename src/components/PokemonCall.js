a0db5833e488eb79f3bd41bcb1db224c91243c17
Anthony <anthonygibson24@gmail.com>
2022-05-23 20:51:54 -0500

    successfully fetched pokemon on render

--- src/components/PokemonCall.js
+++ src/components/PokemonCall.js
@@ -2,21 +2,24 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 
 const PokemonCall = (props) => {
-    const [pokeList, setPokeList]= useState("")
+    const [pokeList, setPokeList]= useState([])
 
     useEffect( () => {
         console.log("hello")
         fetch('https://pokeapi.co/api/v2/pokemon/')
         .then(response => response.json())
         .then(response => setPokeList(response.results))
-
+        .catch(err => console.log(err))
     }, [])
 
     return (
         <div>
+            <form onSubmit={PokemonCall}>
+                <button>Get Pokemon</button>
             {pokeList.map( (pokemon, index) => {
                 return (<p key={index}>{pokemon.name}</p>)
             })}
+            </form>
         </div>
     )
 }
